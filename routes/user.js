c52c01009dcd9ae43b03eaa5ad48125e8cff6ca6
이용현 <yh9035926@naver.com>
2024-05-07 06:15:31 +0900

    isNotLoggedIn

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
 const UserService = require("../service/user");
 
-router.post("/signup", isNotLoggedIn, UserService.signUp);
+router.post("/signup", UserService.signUp);
 router.post("/login", UserService.logIn);
 router.post("/logout", isLoggedIn, UserService.logOut);
 /* router.get("/", UserService.main); */
