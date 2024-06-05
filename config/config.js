af34d7773f73f8de02b72ea905be654d010b5da4
이용현 <yh9035926@naver.com>
2024-06-05 14:53:04 +0900

    mysql ->postgreSQL

--- config/config.js
+++ config/config.js
@@ -4,27 +4,48 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: "root",
-    password: process.env.DB_PASSWORD,
-    database: "blog",
-    host: "svc.sel5.cloudtype.app",
-    port: "32164",
-    dialect: "mysql",
+    port: 5432,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    ssl: "require",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // 추가 옵션 - SSL 인증서가 신뢰할 수 있는 인증서인지 확인하지 않습니다.
+      },
+    },
   },
   test: {
-    username: "root",
-    password: process.env.DB_PASSWORD,
-    database: "blog",
-    host: "svc.sel5.cloudtype.app",
-    port: "32164",
-    dialect: "mysql",
+    port: 5432,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    ssl: "require",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
-    username: "root",
-    password: process.env.DB_PASSWORD,
-    database: "blog",
-    host: "svc.sel5.cloudtype.app",
-    port: "32164",
-    dialect: "mysql",
+    port: 5432,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    ssl: "require",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
