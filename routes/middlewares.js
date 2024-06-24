57be73b7d8e4b02bc1c3bf2c197cccda8b0beaba
이용현 <yh9035926@naver.com>
2024-06-25 08:22:34 +0900

    req.cookies.accessToken

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,32 +1,18 @@
 const jwt = require("jsonwebtoken");
 
 exports.isLoggedIn = (req, res, next) => {
-  const headerCookies = req.headers.cookie;
-
-  const getCookieValue = (cookieString, cookieName) => {
-    const name = `${cookieName}=`;
-    const parts = cookieString.split(";");
-    for (let part of parts) {
-      part = part.trim();
-      if (part.startsWith(name)) {
-        return part.substring(name.length, part.length);
-      }
-    }
-    return null;
-  };
-
-  const accessToken = getCookieValue(headerCookies, "accessToken");
-
-  if (!headerCookies) {
-    return res.status(401).send("헤더에 cookie가 없습니다.");
+  if (!req.cookies.accessToken) {
+    return res.status(401).send("헤더에 쿠키가 없습니다.");
   }
+  const tokenCookie = req.cookies.accessToken;
 
-  if (accessToken) {
-    jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
+  if (tokenCookie) {
+    jwt.verify(tokenCookie, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
         return res.status(401).send("유효하지 않은 토큰입니다.");
       } else {
-        req.user = decoded; // 디코딩된 페이로드를 req.user에 저장
+        req.user = decoded;
+        console.log(decoded); // 디코딩된 페이로드를 req.user에 저장하여 다음 핸들러에서 사용할 수 있도록 합니다.
         next();
       }
     });
@@ -36,10 +22,8 @@ exports.isLoggedIn = (req, res, next) => {
 };
 
 exports.isNotLoggedIn = (req, res, next) => {
-  const authHeader = req.headers["Authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-
-  if (!token) {
+  const tokenCookie = req.cookies.accessToken;
+  if (!tokenCookie) {
     next();
   } else {
     res.status(401).send("이미 로그인한 사용자는 접근할 수 없습니다.");
