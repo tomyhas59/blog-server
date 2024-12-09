f890563fd0d18a53062bc02462195d443b16a8d0
이용현 <yh9035926@naver.com>
2024-12-09 12:36:17 +0900

    $env:NODE_ENV="production"

--- src/migrations/addPostId.js
+++ src/migrations/addPostId.js
@@ -2,11 +2,11 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn("notifications", "CommentId", {
+    await queryInterface.addColumn("notifications", "PostId", {
       type: Sequelize.INTEGER,
       allowNull: true, // Optional: true로 설정하면 NULL 값을 허용
       references: {
-        model: "comments", // 연관된 테이블 이름
+        model: "posts", // 연관된 테이블 이름
         key: "id", // 연관된 테이블의 컬럼 이름
       },
       onUpdate: "CASCADE",
@@ -15,9 +15,8 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn("notifications", "CommentId");
+    await queryInterface.removeColumn("notifications", "PostId");
   },
 };
 
 //npx sequelize-cli db:migrate
-//$env:NODE_ENV="production"; npx sequelize-cli db:migrate
